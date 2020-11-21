7e9e107df87b395951ae9dbf24840c53a3f15a3e
RenataJesus <55641238+RenataJesus@users.noreply.github.com>
2020-11-21 14:33:51 -0300

    .

---
+++ Aula - Algoritimo/71- Exercício.cpp
@@ -0,0 +1,37 @@
+#include<stdlib.h>
+ #include<math.h>
+ #include<locale.h>
+ #include <string.h>
+ 
+  /*
+Exercício 71
+• Criar um algoritmo que receba dois números e imprima os números
+compreendidos entre estes números.
+ */
+ 
+  
+main(){
+	setlocale(LC_ALL, "Portuguese");
+  	
+	int contador, number1, number2;
+	printf("Digite um número: ");
+	scanf ("%d", &number1);
+	printf("Digite outro número: ");
+	scanf ("%d", &number2);
+
+		
+		for(contador=number1+1; contador<=number2; contador++){
+		
+			printf("%d, ", contador);
+		}
+			
+		
+		
+}
+			
+			
+       	
+
+
+  
+ 
---
+++ Aula - Algoritimo/72- Exercício.cpp
@@ -0,0 +1,35 @@
+#include<stdlib.h>
+ #include<math.h>
+ #include<locale.h>
+ #include <string.h>
+ 
+  /*Exercício 72
+• Criar um algoritmo que receba dois números e imprima a soma dos valores
+compreendidos entre estes números.*/
+ 
+  
+main(){
+	setlocale(LC_ALL, "Portuguese");
+  	
+	int contador, number1, number2, soma;
+	printf("Digite um número: ");
+	scanf ("%d", &number1);
+	printf("Digite outro número: ");
+	scanf ("%d", &number2);
+
+		
+		for(contador=number1+1; contador<=number2; contador++){
+		soma=soma+contador;
+			printf("%d, ", soma);
+		}
+			
+		
+		
+}
+			
+			
+       	
+
+
+  
+ 
