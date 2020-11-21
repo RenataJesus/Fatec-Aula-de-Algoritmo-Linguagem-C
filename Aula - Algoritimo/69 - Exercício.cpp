f22c012a3e46f7034c38ba1b2e7548a14cb6504b
RenataJesus <55641238+RenataJesus@users.noreply.github.com>
2020-11-21 11:46:08 -0300

    Create 69 - Exercício.cpp

---
+++ Aula - Algoritimo/69 - Exercício.cpp
@@ -0,0 +1,33 @@
+#include<stdlib.h>
+ #include<math.h>
+ #include<locale.h>
+ #include <string.h>
+ 
+  /*
+Exercício 68 - Criar um algoritmo para imprimir a soma de todos os números de 0 à 100.iba na tela o valor a ser pago ao funcionário,
+adicionando 10% sobre o valor calculado.
+ */
+ 
+  
+main(){
+	setlocale(LC_ALL, "Portuguese");
+  	
+	int contador = 0, number;
+	printf("Digite um número: ");
+	scanf ("%d", &number);
+	
+
+             while( contador <= number )
+             {
+               printf( "%d, ", contador);
+               contador += 1;
+             }
+
+             printf("ACABOU !!!!\n");
+
+	
+}
+
+
+  
+ 
