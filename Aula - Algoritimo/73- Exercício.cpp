6d3c315117278f1fce19b2b79f689ff3666bb6a5
RenataJesus <55641238+RenataJesus@users.noreply.github.com>
2020-11-21 19:57:38 -0300

    Create 73- Exercício.cpp

---
+++ Aula - Algoritimo/73- Exercício.cpp
@@ -0,0 +1,38 @@
+#include<stdlib.h>
+ #include<math.h>
+ #include<locale.h>
+ #include <string.h>
+ 
+  /*Exercício 72
+• Criar um algoritmo que receba dois números e imprima a soma dos valores
+compreendidos entre estes números.*/
+ 
+  
+main(){
+	setlocale(LC_ALL, "Portuguese");
+  	
+	int contador, number1, number2, soma;
+	printf("Digite um número: ");
+	scanf ("%d", &number1);
+	printf("Digite outro número: ");
+	scanf ("%d", &number2);
+
+		
+		for(contador=number1+1; contador<=number2; contador++){
+			soma=soma+contador;
+			if(soma%2==0){
+			printf("%d, ",soma);	
+			}
+		
+		}
+			
+		
+		
+}
+			
+			
+       	
+
+
+  
+ 
